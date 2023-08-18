342c5e49c67cb75708f80b1e8872b9c527b319b2
Luvitor <luan.silva524@etec.sp.gov.br>
2023-08-17 21:47:14 -0300

    janela de menu janeda sobre

---
+++ app/src/main/java/br/com/etecia/myapp/Main_Menu_Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main_Menu_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu_layout);
+    }
+}
